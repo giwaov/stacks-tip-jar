5a63bcb908b41715a463b82775c88776df4a4e1b
giwaov <giwavictor9@gmail.com>
2026-03-04 09:47:09 +0100

    feat: Add talentapp verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,9 @@ export const metadata: Metadata = {
     description: "Support your favorite creators with STX on Stacks blockchain.",
     creator: "@giwaov",
   },
+  other: {
+    "talentapp:project_verification": "b6300f90e937cc3799b7decc1392ba16aaf00581b3775472f383f9ea3014b0abe9bd430bbb995b9b6d6e95c766455e2486c80615594344597927fec341a6",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
