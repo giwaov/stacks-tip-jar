3661a86df8896377039176d65e8aeecf7edb3bb4
giwaov <giwavictor9@gmail.com>
2026-03-04 11:53:15 +0100

    feat: Add components barrel export

---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { Button } from './Button'; export { Card } from './Card'; export { Input } from './Input'; export { Badge } from './Badge'; export { ProgressBar } from './ProgressBar'; export { Avatar } from './Avatar'; export { Toast } from './Toast'; export { Modal } from './Modal'; export { Skeleton } from './Skeleton';
