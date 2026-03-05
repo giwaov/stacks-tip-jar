6189fec244fdfc1880b3342f738be4dc2687c95d
giwaov <giwavictor9@gmail.com>
2026-03-05 09:18:02 +0100

    feat(config): add app constants

---
+++ config/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = 'Stacks Tip Jar';
+export const CONTRACT = { ADDRESS: 'SP3E0DQAHTXJHH5YT9TZCSBW013YXZB25QFDVXXWY', NAME: 'tip-jar-v3' };
+export const LIMITS = { MIN_TIP: 0.001, MAX_TIP: 1000 };
