625ac5e3ed2461b46f96dc9911fd567802ba08d6
giwaov <giwavictor9@gmail.com>
2026-03-05 09:18:12 +0100

    feat(hooks): add useOnClickOutside

---
+++ hooks/useOnClickOutside.ts
@@ -0,0 +1,2 @@
+import { useEffect, RefObject } from 'react';
+export function useOnClickOutside<T extends HTMLElement>(ref: RefObject<T>, handler: () => void) { useEffect(() => { const listener = (e: any) => { if (!ref.current?.contains(e.target)) handler(); }; document.addEventListener('mousedown', listener); return () => document.removeEventListener('mousedown', listener); }, [ref, handler]); }
